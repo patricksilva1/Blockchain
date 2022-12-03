a9ccc4b73f8d1911842aa438f8f164a66b6d78c9
patricksilva1 <patricksilva5160@gmail.com>
2022-12-03 16:29:56 -0300

    adding constansts; Genesis block, Reward and Difficulty

--- src/Constants.java
+++ src/Constants.java
@@ -1,3 +1,14 @@
 public class Constants {
-    
+    private Constants() {
+
+    }
+
+    // Difficulty of leading zeros
+    public static final int DIFFICULTY = 1;
+
+    // This is the reward in the underlying Cryptocurrency
+    public static final int REWARD = 10;
+
+    // Fist Block (Genesis block)
+    public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
 }
