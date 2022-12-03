acd175c425d379a2bb5d175e8c0a68f99a9c9274
patricksilva1 <patricksilva5160@gmail.com>
2022-12-03 20:36:21 -0300

    Implementing the Blockchain

--- src/BlockChain.java
+++ src/BlockChain.java
@@ -1,3 +1,36 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class BlockChain {
-    
+
+    // Immutable Ledger
+    // Here we are not able to remove blocks
+    private List<Block> blockChain;
+
+    public BlockChain() {
+        this.blockChain = new LinkedList<>();
+    }
+
+    public void addBlock(Block block) {
+        this.blockChain.add(block);
+    }
+
+    public List<Block> getBlockChain() {
+        return this.blockChain;
+    }
+
+    public int getSize() {
+        return this.blockChain.size();
+    }
+
+    @Override
+    public String toString() {
+        String s = "";
+
+        for (Block block : this.blockChain) {
+            s += block.toString() + "\n";
+        }
+
+        return s;
+    }
 }
