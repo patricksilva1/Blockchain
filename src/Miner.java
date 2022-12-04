e7da18044d5d1cef1aca7953adc457c7232de7ab
patricksilva1 <patricksilva5160@gmail.com>
2022-12-03 22:20:43 -0300

    Proof of Work

--- src/Miner.java
+++ src/Miner.java
@@ -1,3 +1,32 @@
 public class Miner {
-    
+
+    private double reward;
+
+    public void mine(Block block, BlockChain blockChain) {
+
+        // Proof of Work (PoW)
+        // It will take a while for miners to find the Hash
+        while (!isGoldenHash(block)) {
+            block.incrementNonce();
+            block.generateHash();
+        }
+
+        System.out.println(block + " has just mined...");
+        System.out.println("Hash is: " + block.getHash());
+
+        blockChain.addBlock(block);
+        reward += Constants.REWARD;
+    }
+
+    private boolean isGoldenHash(Block block) {
+
+        String leadingZeros = new String(new char[Constants.DIFFICULTY]).replace('\0', '0');
+
+        return block.getHash().substring(0, Constants.DIFFICULTY).equals(leadingZeros);
+    }
+
+    public double getReward() {
+        return this.reward;
+    }
+
 }
